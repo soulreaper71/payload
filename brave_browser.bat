9ec2f5b5c5165149b5c4837ad5b606e8520fe731
soulreaper71 <141745814+soulreaper71@users.noreply.github.com>
2025-04-20 19:45:57 +0530

    Update brave_browser.bat

--- brave_browser.bat
+++ brave_browser.bat
@@ -2,7 +2,7 @@
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 
 if exist "%userprofile%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/%~n0%~x0" (
-start "" https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start "" https://youtu.be/xvFZjo5PgG0?si=7EvheP5qV0sNY2sa
 pause
 
 ) else (
